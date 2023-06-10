8921bd55b2108a29fc634360f37ed29d4fa5ec8d
Adriana <adrianapardo1998@gmail.com>
2023-06-10 23:00:42 +0200

    added print OXO functionalities

--- js/main.js
+++ js/main.js
@@ -3,20 +3,22 @@ let player2Name = "Melisa";
 const p1Name = "p1N";
 const p2Name = "p2N";
 const input1 = document.getElementById("inputPlayer1");
+const input2 = document.getElementById("inputPlayer2");
+
+sessionStorage.clear();
 
 input1.addEventListener("input", updateValue);
+input2.addEventListener("input", updateValue2);
 
 function updateValue() {
   sessionStorage.removeItem(p1Name);
   sessionStorage.setItem(p1Name, input1.value);
 }
-console.log(player1Name);
+function updateValue2() {
+  sessionStorage.removeItem(p2Name);
+  sessionStorage.setItem(p2Name, input2.value);
+}
 
 document.getElementById("startButton").addEventListener("click", () => {
-  sessionStorage.setItem(p1Name, player1Name);
-  sessionStorage.setItem(p2Name, player2Name);
   window.location.assign("playground.html");
 });
-
-console.log(player1Name);
-console.log(player2Name);
--- js/playground.js
+++ js/playground.js
@@ -2,9 +2,59 @@ let player1Name;
 let player2Name;
 const p1Name = "p1N";
 const p2Name = "p2N";
+const p1NameID = document.getElementById("p1Name");
+const p2NameID = document.getElementById("p2Name");
 
-document.getElementById("p1Name").innerHTML = sessionStorage.getItem(p1Name);
-document.getElementById("p2Name").innerHTML = sessionStorage.getItem(p2Name);
+p1NameID.innerHTML = sessionStorage.getItem(p1Name);
+p2NameID.innerHTML = sessionStorage.getItem(p2Name);
 
 console.log(sessionStorage.getItem(p1Name));
 console.log(sessionStorage.getItem(p2Name));
+
+const tiles = document.getElementsByClassName("tile");
+const playerX = "X";
+const playerO = "O";
+let turno = true;
+
+// coge el valor del id de cada tile
+/* const clicking = (t) => {
+  tileElegido = t.currentTarget;
+  tileID = tileElegido.id;
+  imprimir(tileID);
+  function imprimir(tileID) {
+    if ((document.getElementById(tileID).innerHTML = " ")) {
+      document.getElementById(tileID).addEventListener("click", () => {
+        document.getElementById(tileID).innerHTML = "X";
+      });
+    }
+  }
+};
+ */
+
+const imprimir = (t) => {
+  let tileElegido = t.currentTarget;
+  let tileID = tileElegido.id;
+  console.log(tileID);
+
+  let tile = document.getElementById(tileID);
+
+  if ((tileElegido.innerHTML = " ")) {
+    if (turno == true) {
+      tile.addEventListener("click", () => {
+        tile.innerHTML = playerX;
+        turno = false;
+        tile.removeEventListener("click", imprimir);
+      });
+    } else {
+      tile.addEventListener("click", () => {
+        tile.innerHTML = playerO;
+        turno = true;
+        tile.removeEventListener("click", imprimir);
+      });
+    }
+  }
+};
+
+for (const t of tiles) {
+  t.addEventListener("click", imprimir);
+}
